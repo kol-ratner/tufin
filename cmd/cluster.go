58bd68c47b996ba4c5f395f65f451bab166e99f2
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-10-31 19:54:44 +0200

    update cluster command

--- cmd/cluster.go
+++ cmd/cluster.go
@@ -4,6 +4,8 @@ Copyright © 2024 Kol Ratner kolratner@gmail.com
 package cmd
 
 import (
+	"log"
+
 	"github.com/kol-ratner/tufin/internal/cluster"
 	"github.com/spf13/cobra"
 )
@@ -18,17 +20,36 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := cluster.Create(); err != nil {
-			panic(err)
-		}
-	},
+	Run: clusterEntrypoint,
 }
 
 func init() {
 	rootCmd.AddCommand(clusterCmd)
 
-	clusterCmd.Flags().IntP("nodes", "n", 0, "Specify how many nodes you want to create")
+	// clusterCmd.Flags().IntP("nodes", "n", 0, "Specify how many nodes you want to create")
+	// clusterCmd.Flags().String("name", "default", "Specify a name for the cluster you want to create")
+}
 
-	clusterCmd.Flags().String("name", "default", "Specify a name for the cluster you want to create")
+func clusterEntrypoint(cmd *cobra.Command, args []string) {
+	msgs := make(chan string)
+	// the done channel signals to the main goroutine that the cluster.Create() function has completed
+	// otherwise our program will continue trying to process messages from the cluster.Create() function and panic
+	done := make(chan bool)
+
+	go func() {
+		if err := cluster.Create(msgs); err != nil {
+			log.Println(err)
+		}
+		done <- true
+	}()
+
+	for {
+		select {
+		case msg := <-msgs:
+			log.Println(msg)
+		case <-done:
+			close(msgs)
+			return
+		}
+	}
 }
--- internal/cluster/cluster.go
+++ internal/cluster/cluster.go
@@ -1,12 +1,13 @@
 package cluster
 
 import (
-	"bytes"
+	"bufio"
 	_ "embed"
 	"errors"
-	"log"
+	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
 	"runtime"
 	"strings"
 )
@@ -23,58 +24,129 @@ var k3dLinuxAmd64 []byte
 //go:embed bin/k3d-linux-arm64
 var k3dLinuxArm64 []byte
 
-func Create() error {
-	var k3dBinary []byte
+type k3dHostInfo struct {
+	os   string
+	arch string
+	bin  *os.File
+}
 
-	log.Println("Detecting your operating system...")
-	log.Printf("Detected OS: %s, ARCH: %s \n", runtime.GOOS, runtime.GOARCH)
+// getK3d provides info on the host's runtime env and based on that,
+// selects a k3d binary that is appropriate for the host
+func getK3d() (*k3dHostInfo, error) {
+	var k3dBinary []byte
 
 	switch runtime.GOOS {
 	case "darwin":
 		if runtime.GOARCH == "arm64" {
 			k3dBinary = k3dDarwinArm64
+		} else {
+			k3dBinary = k3dDarwinAmd64
 		}
-		k3dBinary = k3dDarwinAmd64
 	case "linux":
 		if runtime.GOARCH == "arm64" {
 			k3dBinary = k3dLinuxArm64
+		} else {
+			k3dBinary = k3dLinuxAmd64
 		}
-		k3dBinary = k3dLinuxAmd64
 	default:
-		return errors.New("Your operating system is not currently supported")
+		return nil, errors.New("your operating system is not currently supported")
 	}
 
 	tmpFile, err := os.CreateTemp("", "k3d-*")
 	if err != nil {
-		return err
+		return nil, err
 	}
-	defer os.Remove(tmpFile.Name())
 
 	// Write binary content and make executable
 	if _, err := tmpFile.Write(k3dBinary); err != nil {
-		return err
+		return nil, err
 	}
 	if err := tmpFile.Chmod(0755); err != nil {
+		return nil, err
+	}
+	defer tmpFile.Close()
+
+	return &k3dHostInfo{
+		os:   runtime.GOOS,
+		arch: runtime.GOARCH,
+		bin:  tmpFile,
+	}, nil
+}
+
+// parseK3dOutput parses the output of the k3d command and returns the message as a string
+// the trick here is to strip the ansi color code from the k3d output so that we can also
+// strip the formatting of the k3d logger - this gives me complete control over the log output for this application
+func parseK3dOutput(line string) string {
+	// Strip ANSI color codes
+	var stripAnsi *regexp.Regexp = regexp.MustCompile(`\x1b\[[0-9;]*m`)
+	cleanLine := stripAnsi.ReplaceAllString(line, "")
+
+	var infoRegex *regexp.Regexp = regexp.MustCompile(`^INFO\[\d+\]\s+(.+)$`)
+	var fataRegex *regexp.Regexp = regexp.MustCompile(`^FATA\[\d+\]\s+(.+)$`)
+
+	if matches := infoRegex.FindStringSubmatch(cleanLine); matches != nil {
+		return matches[1]
+	}
+
+	if matches := fataRegex.FindStringSubmatch(cleanLine); matches != nil {
+		return matches[1]
+	}
+	// Default to stdout for other messages
+	return line
+}
+
+// create creates a k3d cluster
+func Create(msgChan chan<- string) error {
+	k3d, err := getK3d()
+	if err != nil {
 		return err
 	}
-	tmpFile.Close()
+	defer os.Remove(k3d.bin.Name())
+	msgChan <- fmt.Sprintf("Detected OS: %s, ARCH: %s", k3d.os, k3d.arch)
 
-	// Execute k3d cluster create
-	command := exec.Command(tmpFile.Name(), "cluster", "create")
-	var stderr bytes.Buffer
-	command.Stdout = os.Stdout
-	command.Stderr = os.Stderr
-
-	if err := command.Run(); err != nil {
-		errorMsg := stderr.String()
-		// Find everything after the first ]
-		if idx := strings.Index(errorMsg, "]"); idx != -1 {
-			cleanError := strings.TrimSpace(errorMsg[idx+1:])
-			return errors.New(cleanError)
-		}
-		// log.Fatal(strings.Split(stderr.String(), " ")[0])
-		// log.Fatal(err)
+	command := exec.Command(k3d.bin.Name(), "cluster", "create")
+	stdout, err := command.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := command.StderrPipe()
+	if err != nil {
+		return err
 	}
 
-	return nil
+	if err := command.Start(); err != nil {
+		// defer close(msgChan)
+		return err
+	}
+
+	// writing to the channel via these goroutines is necessary
+	// because of how exec.Command's StdoutPipe() and StderrPipe() work..
+	// they need to be continuously read to prevent the command from blocking
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			text := scanner.Text()
+
+			// only sending specific status updates
+			if strings.Contains(text, "Starting cluster") {
+				msgChan <- parseK3dOutput(text)
+			} else if strings.Contains(text, "created successfully") {
+				msgChan <- parseK3dOutput(text)
+			}
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			text := scanner.Text()
+
+			// only sending specific status updates
+			if strings.Contains(text, "Failed") {
+				msgChan <- parseK3dOutput(text)
+			}
+		}
+	}()
+
+	return command.Wait()
 }
